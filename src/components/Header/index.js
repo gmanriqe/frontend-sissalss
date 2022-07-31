0157c06cc6713074c652b62a4e90e344bd373cb5
gmanriqe <gmanriqe@gmail.com>
2022-07-31 07:28:01 -0500

    TAILWIND - avance layout 1

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,2 +1,23 @@
-const Header = () => <div>Header</div>
+import avatar from '../../assets/images/avatar_profile.jpeg';
+
+const Header = () => {
+    return (
+        <header className='header flex z-20 shadow-md sticky top-0'>
+            <div className='flex p-0 min-h-48 md:min-h-64'>
+                <div className='flex flex-1 px-16'></div>
+                <div className='flex items-center px-8 h-full overflow-x-auto'>
+                    <button type='button' className='profile-header flex items-center min-h-40 min-w-40 px-0 md:px-16 py-0 md:py-6 muiltr-w3cj8e-MuiButtonBase-root-MuiButton-root'>
+                        <div className='hidden md:flex flex-col mx-4 items-end'>
+                            <span className='font-semibold flex muiltr-1niqtu4-MuiTypography-root'>Abbott Keitch</span>
+                            <p className='text-11 font-medium capitalize'>admin</p>
+                        </div>
+                        <div className='profile-header__image md:mx-4'>
+                            <img className='MuiAvatar-img muiltr-1pqm26d-MuiAvatar-img' alt='avatar' src={avatar} />
+                        </div>
+                    </button>
+                </div>
+            </div>
+        </header>
+    )
+}
 export default Header;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 // 1ero: Paquetes de terceros
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 // 2do: Paquetes de mi propio proyecto
 import Client from '../views/Client';
@@ -7,18 +7,16 @@ import Quote from '../views/Quote';
 import Login from '../views/Login';
 import { Layout1, Layout2 } from '../views/Layout';
 
-
 const RoutesComponent = () => (
-        <Routes>
-            <Route element={<Layout1 />} >
-                <Route path="/cliente" element={<Client />} />
-                <Route path="/cita" element={<Quote />} />
-                <Route path="*" element={<div>404</div>} />
-            </Route>
-            <Route element={<Layout2 />} >
-                <Route path="/login" element={<Login />} />
-            </Route>
-        </Routes>
+    <Routes>
+        <Route element={<Layout1 />} >
+            <Route path="/cliente" element={<Client />} />
+            <Route path="/cita" element={<Quote />} />
+            <Route path="*" element={<div>404</div>} />
+        </Route>
+        <Route element={<Layout2 />} >
+            <Route path="/login" element={<Login />} />
+        </Route>
+    </Routes>
 )
-
 export default RoutesComponent;
