3e51d7cb396d48085510c9c31e7123422a7da632
gmanriqe <gmanriqe@gmail.com>
2022-08-17 00:52:15 -0500

    IMPORTANDO SLICE A STORE

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
+
 import authReducer from '../redux/task/auth/authSlice';
+import profileReducer from '../redux/task/profile/profileSlice';
+
 export const store = configureStore({
     reducer: {
-        auth: authReducer
+        auth: authReducer, // 1er reducer
+        profile: profileReducer, // 2do reducer
     }
 });
