b59f9689cd71710aa16777a3c7a49afc1626e982
gmanriqe <gmanriqe@gmail.com>
2022-08-23 14:06:41 -0500

    REDUX RTK - SETUP 2 - SLICE

---
+++ src/redux/slice/customer/customerSlice.js
@@ -0,0 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    data: []
+}
+
+export const customerSlice = createSlice({
+    name: "$customers",
+    initialState,
+    reducers: {
+        /*
+        // actions LIST
+        AddCustomer: (state, action) => {
+            console.log(state, action)
+        }
+        */
+
+        // actions LIST
+        listCustomer: (state, action) => {
+            // return a new data
+            // console.log(action.payload)
+            console.log(state)
+
+            // return [...state, action.payload]
+            return {
+                ...state,
+                data: action.payload
+            }
+        }
+    },
+})
+
+// this is for dispatch
+export const { listCustomer } = customerSlice.actions;
+
+// this is for configureStore
+export default customerSlice.reducer;
+
+
+/*
+ * Luego de crear un slice, se debe importar en el store 
+ */
