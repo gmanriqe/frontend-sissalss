6b9d3a59ebf8ae79223565b7bffacc18645e821a
gmanriqe <gmanriqe@gmail.com>
2022-08-23 14:04:55 -0500

    REDUX RTK - SETUP 1

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import authReducer from '../slice/auth/authSlice';
+import customerReducer from '../slice/customer/customerSlice';
 import profileReducer from '../reducers/profile';
 
 export const store = configureStore({
     reducer: {
         auth: authReducer, // 1er reducer
-        profile: profileReducer, // 2do reducer  
+        profile: profileReducer, // 2do reducer 
+        customer: customerReducer, // 3er reducer  
     }
 });
